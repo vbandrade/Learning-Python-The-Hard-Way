9d2419877f29ceae8f89bb37e2139da1e7ddc506
@vbandrade <vbandrade@gmail.com>
2011-09-29 21:32:34 -0300

    Exercise 05, Extra 04 - Done

---
+++ Exercise 05/ex05-Extra04.py
@@ -0,0 +1,10 @@
+kg_to_lbs = 2.2046 # 1 kg = 2.2046 lbs
+
+weight_in_kg = 95.5
+
+m_to_ft = 3.2808 # 1 m = 3.2808 ft
+
+height_in_m = 1.94
+
+print "I'm %.2f kilos heavy. That's %2.f pounds. " %(weight_in_kg, weight_in_kg * kg_to_lbs )
+print "I'm %.2f m tall. That's %2.f ft. " %(height_in_m, height_in_m * m_to_ft )
